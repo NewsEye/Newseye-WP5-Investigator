9a720f32c0588fec7aa586c7f15ca6fc4a4463fd
Jari Avikainen <jari.avikainen@helsinki.fi>
2019-01-16 14:24:54 +0200

    ENH: Add support for the timestamp fields to the Task objects

--- src/assistant/task.py
+++ src/assistant/task.py
@@ -6,10 +6,12 @@ class Task(dict):
 
     __delattr__ = dict.__delitem__
 
-    def __init__(self, task_id=None, task_type=None, task_parameters=None, result=None, parent_id=None, username=None):
+    def __init__(self, task_id=None, task_type=None, task_parameters=None, result=None, parent_id=None, username=None, created_on=None, last_updated=None):
         self['task_id'] = task_id
         self['task_type'] = task_type
         self['task_parameters'] = task_parameters
         self['result'] = result
         self['parent_id'] = parent_id
         self['username'] = username
+        self['created_on'] = created_on
+        self['last_updated'] = last_updated
