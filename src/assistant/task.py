063da8fc934d99792e6b3a17746731f512dd8681
Jari Avikainen <jari.avikainen@helsinki.fi>
2019-02-14 16:33:57 +0200

    CLEANUP

--- src/assistant/task.py
+++ src/assistant/task.py
@@ -6,14 +6,11 @@ class Task(dict):
 
     __delattr__ = dict.__delitem__
 
-    def __init__(self, task_id=None, task_type=None, task_parameters=None, task_status="created", task_result=None, result_id=None, parent_id=None, username=None, created_on=None, last_updated=None):
+    def __init__(self, task_id=None, task_type=None, task_parameters=None, task_status="created", task_result=None, parent_id=None, username=None):
         self['task_id'] = task_id
         self['task_type'] = task_type
         self['task_parameters'] = task_parameters
         self['task_status'] = task_status
         self['task_result'] = task_result
-        self['result_id'] = result_id
         self['parent_id'] = parent_id
         self['username'] = username
-        self['created_on'] = created_on
-        self['last_updated'] = last_updated
