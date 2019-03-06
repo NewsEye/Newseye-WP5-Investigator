bd0abc43adf86f3a86bb9cdea1cef58b9b7c37a0
Jari Avikainen <jari.avikainen@helsinki.fi>
2019-03-06 16:57:51 +0200

    FIX: Enable running the app from PyCharm

--- investigator.py
+++ investigator.py
@@ -9,9 +9,9 @@ def make_shell_context():
     return {'db': db, 'User': User, 'Query': Query, 'Task': Task}
 
 
-# def main():
-#     app.run(host='0.0.0.0')
-#
-#
-# if __name__ == '__main__':
-#     main()
+def main():
+    app.run(host='0.0.0.0')
+
+
+if __name__ == '__main__':
+    main()
