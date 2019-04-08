8a461347563881bf7d2678cf883abf4c26b7a4b4
Jari Avikainen <jari.avikainen@helsinki.fi>
2019-04-08 16:04:35 +0300

    FIX: Improve the API to better match the report documentation

--- app/analysis/routes.py
+++ app/analysis/routes.py
@@ -1,40 +1,41 @@
 from flask import request, jsonify, current_app
-from flask_login import login_required
+from flask_login import login_required, current_user
 from app.main import core
 from app.analysis import bp
 from app.models import Task
 from app.main.analysis_utils import UTILITY_LIST
 
 
-@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/', methods=['GET'])
 @login_required
-def analyze():
-    if request.method == 'GET':
-        query = request.args.to_dict()
-        query = ('analysis', query) if query.get('target_uuid') or query.get('target_search') else None
-    if request.method == 'POST':
-        query = request.json
-        if isinstance(query, list):
-            query = [('analysis', item) for item in query if item.get('target_uuid') or item.get('target_search')]
-            if not query:
-                return '''Target data is not specified. Include either a 'target_uuid' or 'target_search' parameter.''', 400
-            for item in query:
-                if item[1].get('utility') is None:
-                    return '''Required parameter 'utility' missing for request {}'''.format(item[1]), 400
-        else:
-            query = ('analysis', query) if query.get('target_uuid') or query.get('target_search') else None
-    if query is None:
+def get_analysis_tasks():
+    tasks = [task.dict(style='result') for task in Task.query.filter_by(user_id=current_user.id, task_type='analysis').all()]
+    if len(tasks) == 1:
+        tasks = tasks[0]
+    return jsonify(tasks)
+
+
+@bp.route('/', methods=['POST'])
+@login_required
+def start_analysis_task():
+    # POST: Start a new analysis task, and return its basic information to the user
+    query = request.json
+    if not isinstance(query, list):
+        query = [query]
+    query = [('analysis', item) for item in query if item.get('target_uuid') or item.get('target_search')]
+    if not query:
         return '''Target data is not specified. Include either a 'target_uuid' or 'target_search' parameter.''', 400
-    if query[1].get('tool') is None:
-        return '''Required parameter 'utility' missing for request {}'''.format(query[1]), 400
+    for item in query:
+        if item[1].get('utility') is None:
+            return '''Required parameter 'utility' missing for request {}'''.format(item[1]), 400
     try:
         results = [task.dict() for task in core.execute_tasks(query)]
+        # If any of the tasks is not finished, the status code is set to 202, otherwise it is 200
+        status = 200
         for task in results:
             if task['task_status'] != 'finished':
                 status = 202
                 break
-        else:
-            status = 200
         if len(results) == 1:
             results = results[0]
         return jsonify(results), status
@@ -48,8 +49,8 @@ def analyze():
 
 @bp.route('/<string:task_id>')
 @login_required
-def analysis(task_id):
-    task = Task.query.filter_by(uuid=task_id).first()
+def get_analysis_task(task_id):
+    task = Task.query.filter_by(uuid=task_id, user_id=current_user.id, task_type='analysis').first()
     if task is None:
         return 'Invalid task_id', 400
     return jsonify(task.dict(style='result'))
@@ -58,5 +59,5 @@ def analysis(task_id):
 # TODO: Do this properly instead of using the hardcoded hack of a tool list
 @bp.route('/utilities/')
 @login_required
-def utilities():
+def get_utility_list():
     return jsonify(UTILITY_LIST), 200
--- app/search/routes.py
+++ app/search/routes.py
@@ -1,36 +1,46 @@
 from flask import request, jsonify, current_app
-from flask_login import login_required
+from flask_login import login_required, current_user
 from app.main import core
+from app.models import Task
 from app.search import bp
 
 
-@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/', methods=['POST'])
 @login_required
-def search():
-    if request.method == 'GET':
-        query = request.args.to_dict(flat=False)
-        # Replace the array under the "q" key, if it contains only one item
-        q = query.get('q')
-        if isinstance(q, list) and len(q) == 1:
-            query['q'] = q[0]
-        query = ('search', query)
-    if request.method == 'POST':
-        query = request.json
-        if isinstance(query, list):
-            query = [('search', item) for item in query]
-        else:
-            query = ('search', query)
+def start_search_task():
+    query = request.json
+    if isinstance(query, list):
+        query = [('search', item) for item in query]
+    else:
+        query = [('search', query)]
     try:
-        results = [task.dict(style='result') for task in core.execute_tasks(query)]
+        results = [task.dict() for task in core.execute_tasks(query)]
+        status = 200
         for task in results:
             if task['task_status'] != 'finished':
                 status = 202
                 break
-        else:
-            status = 200
         if len(results) == 1:
             results = results[0]
         return jsonify(results), status
     except Exception as e:
         current_app.logger.exception(e)
         return 'Something went wrong...', 500
+
+
+@bp.route('/', methods=['GET'])
+@login_required
+def get_search_tasks():
+    tasks = [task.dict(style='result') for task in Task.query.filter_by(user_id=current_user.id, task_type='search').all()]
+    if len(tasks) == 1:
+        tasks = tasks[0]
+    return jsonify(tasks)
+
+
+@bp.route('/<string:task_id>')
+@login_required
+def get_search_task(task_id):
+    task = Task.query.filter_by(uuid=task_id, user_id=current_user.id, task_type='search').first()
+    if task is None:
+        return 'Invalid task_id', 400
+    return jsonify(task.dict(style='result'))
