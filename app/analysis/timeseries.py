f5274c5085aa3ae095130cd933845115a90c21a1
Pivovarova Lidia <pivovaro@lx8-500-112.pc.helsinki.fi>
2019-05-07 12:10:26 +0300

    MINOR, FIX

--- app/analysis/timeseries.py
+++ app/analysis/timeseries.py
@@ -47,17 +47,17 @@ def compare_word_to_group(corpus, word, group, item="lemma", granularity="month"
 
 def find_group_kl(corpus, group, item="lemma", granularity="month", min_count = 10):
     ts, ts_ipm = corpus.timeseries(item=item, granularity=granularity, min_count = min_count, word_list=group)
-    group_ts_ipm = corpus.sum_up_timeseries({w:ts_ipm[w] for w in group})        
+    group_ts_ipm = sum_up({w:ts_ipm[w] for w in group})        
     total = corpus._timeseries[item][granularity]['total']
 
     assessment.align_dicts_from_to(total, group_ts_ipm)
     group_dist = ts_to_dist(group_ts_ipm)
 
-    for w in ts_ipm: align_dicts_from_to(total, ts_ipm[w]) 
+    for w in ts_ipm: assessment.align_dicts_from_to(total, ts_ipm[w]) 
     return {w:assessment.kl_divergence(ts_to_dist(ts_ipm[w]), group_dist) for w in ts_ipm}
 
 
-def find_group_outlier(corpus, group, item="lemma", granularity="month", min_count = 10, weights = None):
+def find_group_outliers(corpus, group, item="lemma", granularity="month", min_count = 10, weights = None):
     kl = (find_group_kl(corpus, group, item="lemma", granularity="month", min_count = 10))
     if weights:
         kl = {w:kl[w]*weights[w] for w in kl}
