308b6127b57a3be1018fac56c6a6913b28181735
Jari Avikainen <jari.avikainen@helsinki.fi>
2019-05-27 15:48:15 +0300

    MINOR Refactor

--- app/analysis/planner.py
+++ app/analysis/planner.py
@@ -11,6 +11,10 @@ import asyncio
 # TODO: planner plans the task according to the task dependencies tree
 #  Later on this will become an investigator
 class Planner(object):
+    """
+    This class is not used for anything. Here as a remainder of the planned structure for the planner, to be removed
+    when TaskPlanner is ready
+    """
     async def plan(self, task):
         results = []
         while not self.satisfied(results):
@@ -36,9 +40,7 @@ class TaskPlanner(object):
     async def execute_user_task(self, task_uuids=None):
 
         tasks = Task.query.filter(Task.uuid.in_(task_uuids)).all()
-        # A quick and dirty fix. In the future we should be able to execute_and_store a list of tasks in parallel.
-        for task in tasks:
-            await self.execute_and_store(task)
+        await self.execute_and_store(tasks)
 
     async def async_analysis(self, tasks):
         """ Generate asyncio tasks and run them, returning when all tasks are done"""
@@ -52,49 +54,54 @@ class TaskPlanner(object):
         current_app.logger.info("Tasks finished, returning results")
         return results
 
-    async def execute_and_store(self, task):
+    async def execute_and_store(self, tasks):
+
+        if not isinstance(tasks, list):
+            tasks = [tasks]
 
         # Todo: delay estimates: based on old runtime history for similar tasks?
         # ToDo: Add timeouts for the results: timestamps are already stored, simply rerun the query if the timestamp
         #  is too old.
 
-        task.task_started = datetime.utcnow()
-        # to update data obtained in previous searches
-        if task.task_result and not task.task_parameters.get('force_refresh'):
-            task.task_status = 'finished'
-            task.task_finished = datetime.utcnow()
-        else:
-            task.task_status = 'running'
-        # Remove the 'force_refresh parameter, if it is set. (We don't really want to store that, especially to the
-        # result object. Note that we need to use a dict comprehension to generate a new dict instead of modifing
-        # an existing one for the changes to be committed properly to database.
-        task.task_parameters = {key: value for key, value in task.task_parameters.items() if key != 'force_refresh'}
+        for task in tasks:
+            task.task_started = datetime.utcnow()
+            # to update data obtained in previous searches
+            if task.task_result and not task.task_parameters.get('force_refresh'):
+                task.task_status = 'finished'
+                task.task_finished = datetime.utcnow()
+            else:
+                task.task_status = 'running'
+            # Remove the 'force_refresh parameter, if it is set. (We don't really want to store that, especially to the
+            # result object. Note that we need to use a dict comprehension to generate a new dict instead of modifing
+            # an existing one for the changes to be committed properly to database.
+            task.task_parameters = {key: value for key, value in task.task_parameters.items() if key != 'force_refresh'}
 
         db.session.commit()
 
-        if task.task_status == 'finished':
-            return
+        tasks_to_perform = [task for task in tasks if task.task_status != 'finished']
 
-        required_task = await self.get_prerequisite_tasks(task)
-        if required_task:
-            new_parameters = {key: value for key, value in task.task_parameters.items()}
-            new_parameters['target_uuid'] = str(required_task.uuid)
-            task.task_parameters = new_parameters
-            db.session.commit()
-            await self.execute_and_store(required_task)
+        # A quick and dirty fix. In the future we should be able to perform the tasks in parallel.
+        for task in tasks_to_perform:
+            required_task = await self.get_prerequisite_tasks(task)
+            if required_task:
+                new_parameters = {key: value for key, value in task.task_parameters.items()}
+                new_parameters['target_uuid'] = str(required_task.uuid)
+                task.task_parameters = new_parameters
+                db.session.commit()
+                await self.execute_and_store(required_task)
 
-        if task.task_type == 'search':
-            # runs searches on the external database
-            search_results = await search_database([task.task_parameters])
-            # stores results in the internal database
-            store_results([task], search_results)
+            if task.task_type == 'search':
+                # runs searches on the external database
+                search_results = await search_database([task.task_parameters])
+                # stores results in the internal database
+                store_results([task], search_results)
 
-        if task.task_type == 'analysis':
-            # waiting for tasks to be done
-            # calls main processing function
-            analysis_results = await self.async_analysis([task])
-            # store in the database
-            store_results([task], analysis_results)
+            if task.task_type == 'analysis':
+                # waiting for tasks to be done
+                # calls main processing function
+                analysis_results = await self.async_analysis([task])
+                # store in the database
+                store_results([task], analysis_results)
 
     async def get_prerequisite_tasks(self, task):
         # TODO: Fix the task history to work in the new way (original task is the parent and everything generated by the planner are under it)
