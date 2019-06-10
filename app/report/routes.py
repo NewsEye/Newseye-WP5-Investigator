1dedfcc39acd16765be3595906fb9d82ced23294
Jari Avikainen <jari.avikainen@helsinki.fi>
2019-06-10 10:23:02 +0300

    FIX: Improve error handling

--- app/report/routes.py
+++ app/report/routes.py
@@ -5,7 +5,7 @@ from app.report import ns
 from app.models import Task, Report
 from app.report.report_utils import generate_report, get_formats, get_languages
 from uuid import UUID
-from werkzeug.exceptions import NotFound
+from werkzeug.exceptions import NotFound, BadRequest
 
 
 @ns.route('/<string:task_uuid>')
@@ -31,6 +31,8 @@ class ReportTask(Resource):
         task = Task.query.filter_by(uuid=task_uuid, user_id=current_user.id).first()
         if task is None:
             raise NotFound('Task {} not found for user {}'.format(task_uuid, current_user.username))
+        if task.task_type == 'search':
+            raise BadRequest('Task {} is a search task. Reports can only be generated from analysis tasks.')
         task_report = Report.query.filter_by(task_uuid=task_uuid, report_format=report_format, report_language=report_language).first()
         if task_report:
             return task_report.report_content
