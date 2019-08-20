060d0064c4727457068c2de2c498e6a279e22389
Pivovarova Lidia <pivovaro@lx8-500-112.pc.helsinki.fi>
2019-08-20 11:08:06 +0300

    MINOR: result_comparison moved to investigators's folder

--- app/analysis/__init__.py
+++ app/analysis/__init__.py
@@ -5,7 +5,7 @@ from app.analysis.analysis_utils import ExtractFacets, CommonFacetValues, Genera
 from app.analysis.topic_models import QueryTopicModel, TopicModelDocumentLinking
 from app.analysis.step_detection import FindStepsFromTimeSeries
 from app.analysis.basic_statistics import ExtractWords, MakeBasicStats, ComputeTfIdf
-from app.analysis.result_comparison import ComparisonUtility
+from app.investigator.result_comparison import ComparisonUtility
 
 ns = Namespace('analysis', description='Analysis operations')
 
